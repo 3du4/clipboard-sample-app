b01b3f4f3572be9b5964efab50699af03ee19c4a
edusan4 <sanchez.eduardo@synergy101.jp>
2015-12-09 13:48:36 +0900

    display copied value in an alert view

--- Clipboard/ViewController.swift
+++ Clipboard/ViewController.swift
@@ -8,18 +8,44 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var texFieldButtton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NSNotificationCenter.defaultCenter().addObserver(
+            self,
+            selector: "copyToClip:",
+            name: UIPasteboardChangedNotification,
+            object: nil)
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @objc func copyToClip(notification: NSNotification){
+        self.showAlertView()
+        
+    }
+    
+    func showAlertView(){
+        let pasteBoard = UIPasteboard.generalPasteboard().string
+        let alert = UIAlertController(title: "alert", message: ""+pasteBoard!+""+" was copied into clipboard", preferredStyle: UIAlertControllerStyle.Alert)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 
+    
+    @IBAction func textFieldButton(sender: AnyObject) {
+        print(textField.text)
+    }
 
 }
 
