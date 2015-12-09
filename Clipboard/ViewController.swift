01464feedc8e7ee184a2bf1f241a60235571148f
edusan4 <sanchez.eduardo@synergy101.jp>
2015-12-09 12:22:00 +0900

    Initial Commit

---
+++ Clipboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Clipboard
+//
+//  Created by Eduardo on 12/9/15.
+//  Copyright © 2015 Eduardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
